14aaf63116bd1568aced2d722ec0caeebcdd64d6
Kragada <35291972+Kragada@users.noreply.github.com>
2021-09-27 10:42:39 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
